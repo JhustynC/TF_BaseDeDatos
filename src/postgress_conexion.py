74c42c421489a274baabc646ffd4789dcea895e3
Jhustyn Carvajal <christopherarias2002@gmail.com>
2024-01-26 21:51:28 -0500

    Cambio en confg db

--- src/postgress_conexion.py
+++ src/postgress_conexion.py
@@ -15,12 +15,36 @@ try:
     
     # Crear un objeto cursor
     cursor = conection.cursor()
-    
-    print('Coneccion realizada')
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'AND table_type = 'BASE TABLE';")
+    filas = cursor.fetchall()
+    for fila in filas:
+        print(fila)
+    #cursor.execute("INSERT INTO ")
+    print()
     
 except Exception as e:
     print(e)
-    
+
+finally:
+    if conection != None:
+        cursor.close()
+        conection.close()
+
+
+
+# Ejecutar una consulta SQL más compleja con JOIN
+consulta_sql = """
+    SELECT clientes.nombre AS nombre_cliente, pedidos.fecha, productos.nombre AS nombre_producto
+    FROM pedidos
+    JOIN clientes ON pedidos.cliente_id = clientes.id
+    JOIN detalles_pedido ON pedidos.id = detalles_pedido.pedido_id
+    JOIN productos ON detalles_pedido.producto_id = productos.id
+    WHERE clientes.nombre = %s;
+"""
+
+
+#cursor.close()
+#conection.close()
     
 #Prueba de cambio para conection 
 
