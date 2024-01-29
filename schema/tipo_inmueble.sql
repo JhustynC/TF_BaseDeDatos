755895fc719024fe81c01b2eb061413865d6ee8f
Jhustyn Carvajal <christopherarias2002@gmail.com>
2024-01-28 21:29:10 -0500

    sql tipo inm

---
+++ schema/tipo_inmueble.sql
@@ -0,0 +1,11 @@
+INSERT INTO tipo_inmueble (id, nombre) VALUES
+('01', 'Casa'),
+('02', 'Apartamento'),
+('03', 'Oficina'),
+('04', 'Local Comercial'),
+('05', 'Terreno'),
+('06', 'Edificio'),
+('07', 'Finca'),
+('08', 'Bodega'),
+('09', 'Piso'),
+('10', 'Chalet');
