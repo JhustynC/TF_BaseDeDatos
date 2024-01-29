9366d01997443328a00e69612e3b39861dcf8d94
Jhustyn Carvajal <christopherarias2002@gmail.com>
2024-01-28 19:36:58 -0500

    Cambio en los CBX y esqumas

---
+++ schema/parroquias.sql
@@ -0,0 +1,63 @@
+-- Inserción de datos en la tabla 'parroquia' para GIRÓN
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('31', 'Girón', '4'),
+('32', 'Asunción', '4'),
+('33', 'San Gerardo', '4');
+
+-- Inserción de datos en la tabla 'parroquia' para Sta. ISABEL
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('41', 'Santa Isabel', '5'),
+('42', 'Adbón Calderón (La unión)', '5'),
+('43', 'Zhaglli (Shaglli)', '5');
+
+-- Inserción de datos en la tabla 'parroquia' para SÍGSIG
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('51', 'Sígsig', '6'),
+('52', 'Cuchil (Cutchil)', '6'),
+('53', 'Jima (Gima)', '6'),
+('57', 'San José de Raranga', '6');
+
+-- Inserción de datos en la tabla 'parroquia' para Sn. FERNANDO
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('61', 'San Fernando Chumblín', '7');
+
+-- Inserción de datos en la tabla 'parroquia' para NABÓN
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('71', 'Nabón', '8'),
+('72', 'Cochapata', '8'),
+('73', 'El Progreso', '8'),
+('77', 'Las Nieves (Chaya)', '8');
+
+-- Inserción de datos en la tabla 'parroquia' para PUCARÁ
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('81', 'Pucará San Rafael de Sharug', '9');
+
+-- Inserción de datos en la tabla 'parroquia' para OÑA
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('91', 'San Felipe de Oña Susudel', '10');
+
+-- Inserción de datos en la tabla 'parroquia' para CHORDELEG
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('101', 'Chordeleg', '11'),
+('102', 'La unión', '11'),
+('103', 'Luis Gallarza Orellana (Delegsol)', '11'),
+('104', 'Principal', '11'),
+('105', 'San Martín de Puzhío', '11');
+
+-- Inserción de datos en la tabla 'parroquia' para EL PAN
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('111', 'El pan', '12'),
+('112', 'San Vicente', '12');
+
+-- Inserción de datos en la tabla 'parroquia' para SEVILLA DE ORO
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('121', 'Sevilla de Oro Amaluza', '13'),
+('122', 'Palmas', '13');
+
+-- Inserción de datos en la tabla 'parroquia' para GUACHAPALA
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('131', 'Guachapala', '14');
+
+-- Inserción de datos en la tabla 'parroquia' para CAMILO PONCE ENRÍQUEZ
+INSERT INTO parroquia (id, nombre, id_ciudad) VALUES
+('141', 'Camilo Ponce Enríquez El Carmen de Pujilí', '15');
