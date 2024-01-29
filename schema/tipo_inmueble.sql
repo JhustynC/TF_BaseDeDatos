2360e40f72bb0877cf04680fd6c80be1b18f84ac
Jorge Buelvas <jorge.buelvas01@ucuenca.edu.ec>
2024-01-29 00:36:41 -0500

    ultimos cambios

--- schema/tipo_inmueble.sql
+++ schema/tipo_inmueble.sql
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-INSERT INTO tipo_inmueble (id, nombre) VALUES
-('01', 'Casa'),
-('02', 'Apartamento'),
-('03', 'Oficina'),
-('04', 'Local Comercial'),
-('05', 'Terreno'),
-('06', 'Edificio'),
-('07', 'Finca'),
-('08', 'Bodega'),
-('09', 'Piso'),
-('10', 'Chalet');
-=======
 
 INSERT INTO tipo_inmueble (id, nombre) VALUES
 ('01','Casa'),
@@ -23,4 +10,3 @@ INSERT INTO tipo_inmueble (id, nombre) VALUES
 ('08','Bodega'),
 ('09','Piso'),
 ('10','Chalet');
->>>>>>> 720e110dfcb949affddaf1a58be7bef8bacd0338
